350caa823f52aa1ab57a1e97360c34a089a9a8c9
DukeeTheProgrammer <ujiroduke1@gmail.com>
2026-01-21 14:58:42 +0100

    Updated Cors issues in fetch

--- dukee_dict/settings.py
+++ dukee_dict/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     "dict_auth",
     "dict",
     'django.contrib.admin',
@@ -42,8 +43,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
